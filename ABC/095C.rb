df1a910015415ec50a4d2b9c6f0b2bb76d46f881
zyeup <160883122+zyeup@users.noreply.github.com>
2024-11-02 23:34:06 +0900

    余計に時間がかかった

--- ABC/095C.rb
+++ ABC/095C.rb
@@ -2,27 +2,23 @@ a, b, c, x, y = gets.chomp.split(" ").map(&:to_i)
 
 result = []
 
-#  a < c < bの場合
-if x > y && b > c && a < c
-  result.push(c * 2 * y + a * (x-y))
-end
-
-# b < c < aの場合
-if x < y && a > c && b < c
-  result.push(c * 2 * x + b * (y-x))
-end
-
 
 result.push(a * x + b * y)
 
-
-# cが極端に小さい場合
 if x >= y
-  result.push(c * 2 * x)
+  result.push(2 * c * x)
 end
 
-if y >= x
-  result.push(c * 2 * y)
+if x <= y
+  result.push(2 * c * y)
+end
+
+if x <= y
+  result.push(2 * c * x + b * (y - x))
+end
+
+if x >= y
+  result.push(2 * c * y + a * (x - y))
 end
 
 puts result.min
