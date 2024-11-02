b6e122266f2babb4689a1a6cefe442d07d5e99c5
zyeup <160883122+zyeup@users.noreply.github.com>
2024-11-02 10:29:58 +0900

    AC 376C

--- ABC376C.rb
+++ ABC376C.rb
@@ -3,23 +3,27 @@ arrayA = gets.chomp.split(" ").map(&:to_i)
 arrayB = gets.chomp.split(" ").map(&:to_i)
 count = []
 error = -1
+sorted_ArrayA = arrayA.sort.reverse!
+sorted_ArrayB = arrayB.sort.reverse!
 
-until arrayB.empty? do
-  if arrayA.max <= arrayB.max
-    arrayA.delete(arrayA.max)
-    arrayB.delete(arrayB.max)
-  else
-    count.push(arrayA.max)
-    arrayA.delete(arrayA.max)
+a_i = 0
+b_i = 0
+
+while b_i <= sorted_ArrayB.size - 1
+  if sorted_ArrayA[a_i] > sorted_ArrayB[b_i]
+    count.push(sorted_ArrayA[a_i])
+    b_i -= 1
   end
   if count.size > 1
-    puts error
-    break
+      puts error
+      break
   end
+  a_i += 1
+  b_i += 1
 end
 
 if count.size == 1
   puts count[0]
 elsif count.size < 1
-  puts arrayA[0]
+  puts sorted_ArrayA[-1]
 end
